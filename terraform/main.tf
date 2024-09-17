83ddb46dc8ddee3cdeababd13a4da54e2f04ae44
AnantVispute <anantvispute@gmail.com>
2024-09-17 17:25:57 +0530

    test7

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,12 +4,12 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "rg" {
-  name     = "myResourceGroup"
+  name     = "myResourceGroup369"
   location = "East US"
 }
 
 resource "azurerm_app_service_plan" "asp" {
-  name                = "myAppServicePlan"
+  name                = "myAppServicePlan369"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   sku {
@@ -19,7 +19,7 @@ resource "azurerm_app_service_plan" "asp" {
 }
 
 resource "azurerm_app_service" "app" {
-  name                = "myWebApp"
+  name                = "myWebApp369"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   app_service_plan_id = azurerm_app_service_plan.asp.id
